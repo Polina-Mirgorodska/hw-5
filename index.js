add5485c437976edd0d74aa40928165d62bd10d6
Polina-Mirgorodska <mirgorodskapolina@gmail.com>
2026-02-07 17:44:54 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const drink = 2; 
+
+switch (choice) {
+  case 1:
+    console.log("Кава");
+    break;
+  case 2:
+    console.log("Чай");
+    break;
+  case 3:
+    console.log("Сік");
+    break;
+  default:
+    console.log("Невірний вибір");
+}
